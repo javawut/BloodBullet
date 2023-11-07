dc8c33f228d79ba4d8b06b6a0ed4cb298a0d2913
GiovaniPS097 <giovani.bq97@hotmail.com>
2023-11-06 21:30:33 -0300

    LifesBar

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    public float health = 2;
+    public float healthMax = 14;
+
+    public Image healthBar;
+
+    public void Update(){
+       UpdateHealthBar()
+    }
+    private void UpdateHealthBar()
+    {
+        healthBar.fillAmount = health / healthMax;
+    }
+}
